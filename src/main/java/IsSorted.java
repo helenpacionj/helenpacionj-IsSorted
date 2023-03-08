4ef273bfe0c2306d8b2a422c063350397b82e846
Helen Pacio <helen.pacio@outlook.com>
2023-03-08 04:09:47 +0000

    Completed Mar-07-2023

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -7,6 +7,11 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+        //return false;
+        
+       for (int ctr=1; ctr < arr.length; ctr++) {
+          if (arr[ctr] < arr[ctr-1]) return false;
+       }
+       return true;
     }
 }
